32413abbe9e0ce846ff30929a61288916f1db7c5
ruanSignori <cleosignori@gmail.com>
2023-04-17 07:30:51 -0300

    mend

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>Olá, seja bem vinda!</h1>
-      <p>A atividade proposta pelo o curso, era pensar em uma pessoa, e desenvolver um site que vai impactar a vida dessa pessoa</p>
-      <p>Como sugestão, ele disse para deixar uma frase para essa pessoa, e então, resolvi fazer uma espécie de "desabafo"</p>
-      <p>Reconheço o esforço que você está fazendo pela gente.</p>
-      <p>E lhe admiro muito por tudo, e te amo acima de tudo.</p>
-      <p>Dias difíceis cheguei a conclusão que sempre vão existir, assim como os dias de glória.</p>
-      <p>E todos os dias, quero ter você aqui comigo, e estar contigo.</p>
-      <p>Você é meu maior tesouro, e tenho muito orgulho de ti, te amo Rebeca!</p>
-      <p>Lhe desejo um ótimo e abençoado dia, se cuida!</p>
+      <h1>Olá, seja bem vindo!</h1>
     </div>
   );
 }
