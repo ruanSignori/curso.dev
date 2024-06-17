a7bc7f6b26088b375b10e719227d8e2274037c18
ruan <cleosignori@gmail.com>
2024-06-16 21:56:12 -0300

    feat: adds `api/v1/migrations` endpoint

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -1,9 +1,19 @@
+import database from "infra/database.js";
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;")
+}
+
+// Antes de executar os testes de migrations, limpa as migrations
+beforeAll(cleanDatabase);
+
 test("GET to /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
 
 
--- test/integration/api/v1/migrations/post.test.js
+++ test/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,25 @@
-test("GET to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
-  expect(response.status).toBe(200);
+import database from "infra/database.js";
 
-  const responseBody = await response.json();
-  expect(Array.isArray(responseBody)).toBe(true);
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA PUBLIC CASCADE; CREATE SCHEMA PUBLIC;")
+}
+
+beforeAll(cleanDatabase);
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST"
+  });
+
+  expect(response.status).toBe(201); 
 });
 
+test("all migrations should runner", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST"
+  });
+
+  const listMigrations = await database.query("SELECT COUNT(*) FROM pgmigrations;");
+  expect(Number(listMigrations.rows[0].count)).toBeGreaterThan(0); 
+})
 
